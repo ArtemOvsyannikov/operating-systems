b2c1f09384cf69359361e3b88c5c0b480484c07e
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-12 16:08:44 +0300

    add --help

--- 1-21.sh
+++ 1-21.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-chmod +x ./1-21.sh
-
-echo -e "шлем\nнагрудник\nпоножи\nботинки\nмеч\nтопор\nлук\nарбалет\nпосох\nкопьё" > items.txt
-echo -e "огня\nводы\nсвета\nветра\nтьмы\nземли\nвсевластия\nльда\nмолнии\nбесконечности" > properties.txt
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "Этот скрипт генерирует случайный инвентарь для игрока."
+    echo "В качестве входных данных используются следующие файлы: items.txt(назв-я предметов), properties.txt(назв-я свойств)."
+    echo "Использование: ./1-21.sh [-h | --help]"
+    exit  0
+fi
 
 echo "Инвентарь:"
-for i in $(seq 1 5)
+for i in $(seq  1  5)
 do
-  item=$(shuf -n 1 "./items.txt")
-  property=$(shuf -n 1 "./properties.txt")
+  item=$(shuf -n  1 "./items.txt")
+  property=$(shuf -n  1 "./properties.txt")
 
   echo "$i. $item $property"
 done
+
+exit  0
