62ea363d7daa01a49cbbb292392928a412da84c8
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-12 16:12:49 +0300

    add files error

--- 1-21.sh
+++ 1-21.sh
@@ -2,18 +2,22 @@
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     echo "Этот скрипт генерирует случайный инвентарь для игрока."
-    echo "В качестве входных данных используются следующие файлы: items.txt(назв-я предметов), properties.txt(назв-я свойств)."
     echo "Использование: ./1-21.sh [-h | --help]"
-    exit  0
+    exit   0
+fi
+
+if [[ ! -f "items.txt" || ! -f "properties.txt" ]]; then
+    echo "Ошибка: Файлы items.txt и properties.txt должны находиться в той же директории, что и этот скрипт."
+    exit  1
 fi
 
 echo "Инвентарь:"
-for i in $(seq  1  5)
+for i in $(seq   1   5)
 do
-  item=$(shuf -n  1 "./items.txt")
-  property=$(shuf -n  1 "./properties.txt")
+  item=$(shuf -n   1 "items.txt")
+  property=$(shuf -n   1 "properties.txt")
 
   echo "$i. $item $property"
 done
 
-exit  0
+exit 0
