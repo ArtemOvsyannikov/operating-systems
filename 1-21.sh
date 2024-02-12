fb4a99ca2e505efb346b43e8ae86e5cf0461b920
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-12 16:18:21 +0300

    1

--- 1-21.sh
+++ 1-21.sh
@@ -3,12 +3,12 @@
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     echo "Этот скрипт генерирует случайный инвентарь для игрока."
     echo "Использование: ./1-21.sh [-h | --help]"
-    exit   0
+    exit 0
 fi
 
 if [[ ! -f "items.txt" || ! -f "properties.txt" ]]; then
     echo "Ошибка: Файлы items.txt и properties.txt должны находиться в той же директории, что и этот скрипт."
-    exit  1
+    exit 1
 fi
 
 echo "Инвентарь:"
