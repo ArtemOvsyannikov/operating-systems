07cffbb23ac390aa421de794268fe8313d57ffc7
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-12 15:49:37 +0300

    merge all files in 1-21.sh

--- 1-21.sh
+++ 1-21.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-chmod +x ./prepairing.sh ./inventory.sh ./1-21.sh
+chmod +x ./1-21.sh
 
-./prepairing.sh
-./inventory.sh
+echo -e "шлем\nнагрудник\nпоножи\nботинки\nмеч\nтопор\nлук\nарбалет\nпосох\nкопьё" > items.txt
+echo -e "огня\nводы\nсвета\nветра\nтьмы\nземли\nвсевластия\nльда\nмолнии\nбесконечности" > properties.txt
+
+echo "Инвентарь:"
+for i in $(seq 1 5)
+do
+  item=$(shuf -n 1 "./items.txt")
+  property=$(shuf -n 1 "./properties.txt")
+
+  echo "$i. $item $property"
+done
